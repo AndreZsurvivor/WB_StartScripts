b5a2dead19f5d3aeb6da3dc1318ef62335d3b0df
AndreZ <zwieback@crazyuniverse.net>
2024-02-24 18:10:27 +0100

    first commit

---
+++ 2start_client.bat
@@ -0,0 +1,3 @@
+cd /D "E:\SteamLibrary\steamapps\common\DayZ"
+
+start DayZDiag_x64.exe -name=AndreZ -window "-mod=E:\SteamLibrary\steamapps\common\DayZ\@stmoritz;P:\azw_ClassicWalk" -forcedebugger -scriptDebug=true -dologs -filePatching -newErrorsAreWarnings=1 -noPause -connect=127.0.0.1 -port=2302
